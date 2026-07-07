857477658ce3be105b498d4762af09fad8dbb019
tcminh1411 <minhdc24v7k522@vlvh.ctu.edu.vn>
2026-07-07 18:40:36 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <p>
+      Oops, không thể tìm thấy trang. Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.page {
+  max-width: 700px;
+  margin: 2rem auto;
+}
+</style>
